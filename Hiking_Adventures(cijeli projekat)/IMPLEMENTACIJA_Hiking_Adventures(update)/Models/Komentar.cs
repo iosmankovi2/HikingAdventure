0cae964831e2bc61f786f6fde9c1cd862c2ae9f3
iosmankovi2 <72872550+iosmankovi2@users.noreply.github.com>
2021-06-14 00:02:17 +0200

    Add files via upload

---
+++ Hiking_Adventures(cijeli projekat)/IMPLEMENTACIJA_Hiking_Adventures(update)/Models/Komentar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hiking_Adventures.Models
+    public class Komentar
+    {
+        public int KomentarId { get; set; }
+        public string tekstKomentara { get; set; }
+
+        //dio u kojem se definisu veze sa ostalim klasama
+        public virtual Vodic Vodic { get; set; }
+        public virtual RegistrovaniKorisnik RegistrovaniKorisnik { get; set; }
+
+    }
+}
---
+++ Hiking_Adventures(cijeli projekat)/IMPLEMENTACIJA_Hiking_Adventures(update)/Models/Vodic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hiking_Adventures.Models
+
+public class Vodic
+{
+	public int VodicId { get; set; }
+	public int List<Ocjena> ocjene;
+	public string List<Komentar> komentari;
+	public Ruta mojaRuta {get; set;}
+
+	public virtual Osoba Osoba { get; set; }
+	
+}
